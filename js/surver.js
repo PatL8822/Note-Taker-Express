324a961e9fb6a3857c337f5b7ac92d135a3cdf02
Isaiah411013 <Archangel_r320@hotmail.com>
2022-10-31 17:29:20 -0700

    structure files better orginizing

---
+++ js/surver.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const api = require('../public/index.js');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
