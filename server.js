ccbda22590862c6b55bdbb8e34ed78b38d9a696c
Isaiah411013 <Archangel_r320@hotmail.com>
2022-11-01 20:56:25 -0700

    adding 404 updating html

--- server.js
+++ server.js
@@ -6,8 +6,19 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+app.get('/public/notes.html', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+);
 
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
